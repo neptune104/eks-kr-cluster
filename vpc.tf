999d8f297200d3cbe57e4d11a155a2a9c22c5d3d
root <neptune104@nate.com>
2024-12-15 21:02:08 +0900

    test-9

--- vpc.tf
+++ vpc.tf
@@ -22,7 +22,7 @@ resource "aws_vpc" "demo" {
 resource "aws_subnet" "demo" {
   count = 2
 
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  #availability_zone       = data.aws_availability_zones.available.names[count.index]
   #availability_zone = element(["ap-northeast-2a", "ap-northeast-2c"], count.index)
   availability_zone = element(["us-west-2a", "us-west-2c"], count.index)
   cidr_block              = "10.0.${count.index}.0/24"
